a0640fa8dfde7ea52b450713531161a145cd7b42
TaisceCheol <piaras.hoban@itma.ie>
2017-01-04 10:14:08 +0000

    modified process.sh to store filelist in /var/tmp

--- process.sh
+++ process.sh
@@ -17,13 +17,20 @@ IFS=$'\n'
 # glob MXF files in subdirectories, expects video folder mounted at /video
 MXF_FILES=$(find $READ_DIR -type f -name '*.MXF')
 
-printf "file '%s'\n" $MXF_FILES > $WRITE_DIR/data.txt
+# make directory to store concat textfile
+FILELIST_PATH=/var/tmp/ffmpeg_concat/
+mkdir -p $FILELIST_PATH
 
+# generate filelist 
+printf "file '%s'\n" $MXF_FILES > $FILELIST_PATH/filelist.txt
+
+# run ffmpeg checking if duration is set
 if [ $DURATION -eq -1 ]
 then
-	ffmpeg -y -f concat -safe 0 -i $WRITE_DIR/data.txt -pix_fmt $PIX_FMT -vf yadif -c:v $VIDEO_CODEC -preset $PRESET -crf $CRF -c:a $AUDIO_CODEC -b:a $AUDIO_BITRATE -ac 2 -map 0 -loglevel error $WRITE_DIR/$NAME.mp4
+	ffmpeg -y -f concat -safe 0 -i $FILELIST_PATH/filelist.txt -pix_fmt $PIX_FMT -vf yadif -c:v $VIDEO_CODEC -preset $PRESET -crf $CRF -c:a $AUDIO_CODEC -b:a $AUDIO_BITRATE -ac 2 -map 0 -loglevel error $WRITE_DIR/$NAME.mp4
 else
-	ffmpeg -y -f concat -safe 0 -i $WRITE_DIR/data.txt -t $DURATION -pix_fmt $PIX_FMT -vf yadif -c:v $VIDEO_CODEC -preset $PRESET -crf $CRF -c:a $AUDIO_CODEC -b:a $AUDIO_BITRATE -ac 2 -map 0 -loglevel error $WRITE_DIR/$NAME.mp4
+	ffmpeg -y -f concat -safe 0 -i $FILELIST_PATH/filelist.txt -t $DURATION -pix_fmt $PIX_FMT -vf yadif -c:v $VIDEO_CODEC -preset $PRESET -crf $CRF -c:a $AUDIO_CODEC -b:a $AUDIO_BITRATE -ac 2 -map 0 -loglevel error $WRITE_DIR/$NAME.mp4
 fi
 
-rm $WRITE_DIR/data.txt
+cat $FILELIST_PATH/filelist.txt
+rm $FILELIST_PATH/filelist.txt
